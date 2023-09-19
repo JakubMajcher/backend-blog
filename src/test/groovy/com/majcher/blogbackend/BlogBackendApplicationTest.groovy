a26b4a47cd0f91357c6a74b4f8720659e6dc9170
Jakub Majcher <jakub.majcher@tesco.com>
2023-09-19 16:56:02 +0200

    ➕ add spock

---
+++ src/test/groovy/com/majcher/blogbackend/BlogBackendApplicationTest.groovy
@@ -0,0 +1,11 @@
+package com.majcher.blogbackend
+
+import spock.lang.Specification
+
+class BlogBackendApplicationTest extends Specification {
+
+    def "first dummy test just to be sure spock is working"() {
+        expect:
+        1 == 1
+    }
+}
